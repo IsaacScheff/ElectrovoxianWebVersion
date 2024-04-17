28445ff71dea8c2ea84796db34483aff94a3501e
Isaac Scheff <isaac1996@comcast.net>
2024-04-17 19:25:04 -0400

    Added collider and sensor to player

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,16 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         let { scene, x, y, texture } = data;
         super(scene.matter.world, x, y, texture);
         this.scene.add.existing(this);
+
+        const { Body, Bodies } = Phaser.Physics.Matter.Matter;
+        var playerCollider = Bodies.circle(this.x, this.y, 20, { isSensor: false, label: 'playerCollider' });
+        var playerSensor = Bodies.circle(this.x, this.y, 32, { isSensor: true, label: 'playerSensor' });
+        const compoundBody = Body.create({
+            parts: [ playerCollider, playerSensor ],
+            frictionAir: 0.35,
+        });
+        this.setExistingBody(compoundBody);
+        this.setFixedRotation();
     }
 
     static preload(scene) {
